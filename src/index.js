197ac303bc01091d8c3d4fb4d8c7e602e6657282
Peyman Jomehri <pjomehri@gmail.com>
2019-09-29 16:59:46 -0400

    adding comment for test!

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
+// Adding a comment for Git
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
